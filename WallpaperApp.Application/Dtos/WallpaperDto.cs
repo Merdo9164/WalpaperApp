d3b30218ef3b10347bfc7754a6ee297682330309
Merdo9164 <153107982+Merdo9164@users.noreply.github.com>
2025-10-13 11:52:46 +0300

    WLLPPRPPDB-28: EF Core packages added to Infrastructure project

---
+++ WallpaperApp.Application/Dtos/WallpaperDto.cs
@@ -0,0 +1,9 @@
+namespace WallpaperApp.Application.Dtos
+{
+    public class WallpaperDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ImageUrl{ get; set; }
+    }
+}
