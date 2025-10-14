d5165112ea824f36783a5f9d07beae62543d8ae3
Merdo9164 <153107982+Merdo9164@users.noreply.github.com>
2025-10-14 16:41:27 +0300

    WLLPPRPPDB-26
    POST endpoint'inden alınan verilerin veritabanına doğru şekilde kaydedilmesini sağlayacak işlem

--- WallpaperApp.Application/Interfaces/IWallpaperRepository.cs
+++ WallpaperApp.Application/Interfaces/IWallpaperRepository.cs
@@ -1,14 +1,10 @@
 using WallpaperApp.Domain.Entities;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace WalpaperApp.Application.Interfaces
 {
     public interface IWallpaperRepository
     {
         Task<IEnumerable<Wallpaper>> GetAllAsync();
-        // Sonraki tasklarda ekleyeceğin metotları buraya ekle
-        Task<Wallpaper> GetByIdAsync(int id);
-        Task AddAsync(Wallpaper wallpaper);
+        Task <Wallpaper> AddAsync(Wallpaper wallpaper); //ekleme metodu
     }
 }
