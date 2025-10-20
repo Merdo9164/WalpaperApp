f3f4040a72b5f4dc784bf828ed24cd7bba923e3d
Merdo9164 <153107982+Merdo9164@users.noreply.github.com>
2025-10-20 10:12:43 +0300

    WLLPPRPPDB-43 :dto-ve-repository-guncellemesi

--- WallpaperApp.Domain/Entities/Wallpaper.cs
+++ WallpaperApp.Domain/Entities/Wallpaper.cs
@@ -2,7 +2,7 @@ namespace WallpaperApp.Domain.Entities
 {
     public class Wallpaper
     {
-        public Guid Id { get; set; } = new Guid.NewGuid();
+        public Guid Id { get; set; } = Guid.NewGuid();
         public string Title { get; set; } = string.Empty;
         public string ImageUrl { get; set; } = string.Empty;
     }
