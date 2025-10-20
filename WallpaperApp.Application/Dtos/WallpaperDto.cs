1c41643c2b982650cfbc73cb388a31efbbce5943
Merdo9164 <153107982+Merdo9164@users.noreply.github.com>
2025-10-20 09:47:57 +0300

    WLLPPRPPDB-42 <entity güncellemesi>

--- WallpaperApp.Application/Dtos/WallpaperDto.cs
+++ WallpaperApp.Application/Dtos/WallpaperDto.cs
@@ -2,7 +2,7 @@ namespace WallpaperApp.Application.Dtos
 {
     public class WallpaperDto
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public string ImageUrl { get; set; } = string.Empty;
     }
